e0515fc6b26380806baa720ea348fbfb72bbc344
Marijan Brvar <marijan.brvar@gmail.com>
2021-04-08 14:29:37 +0200

    fix offenses

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -11,5 +10,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:commenter, :body)
   end
-
 end
