6b894e9ad67da8da980eb61e9e1397f4e05c3891
Marijan Brvar <marijan.brvar@gmail.com>
2021-04-08 12:17:47 +0200

    add update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -21,7 +21,23 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+
+  end
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
     def article_params
       params.require(:article).permit(:title, :body)
